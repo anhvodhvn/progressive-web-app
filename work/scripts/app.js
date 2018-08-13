ee5827b0a8c7591ff035c785fd9024fab8a955e3
devinsg <duyanh2005@gmail.com>
2018-08-13 10:29:54 +0700

    save selectedCities to localStorage

--- work/scripts/app.js
+++ work/scripts/app.js
@@ -191,6 +191,10 @@
   };
 
   // TODO add saveSelectedCities function here
+  app.saveSelectedCities = function() {
+    var selectedCities = JSON.stringify(app.selectedCities);
+    localStorage.selectedCities = selectedCities;
+  };
 
   app.getIconClass = function(weatherCode) {
     // Weather codes: https://developer.yahoo.com/weather/documentation.html#codes
